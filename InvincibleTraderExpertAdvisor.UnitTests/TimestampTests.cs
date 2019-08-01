9c3b73e73c284823858c9e632c6bc209d8cf215a
ronaldo-morillo <ronaldo.morillo@aurea.com>
2019-08-01 12:30:11 +0800

    Added Timestamp

---
+++ InvincibleTraderExpertAdvisor.UnitTests/TimestampTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace InvincibleTraderExpertAdvisor.UnitTests
+{
+    public class TimestampTests
+    {
+        [Fact]
+        public void FromDateTimeConvertsDateTimeToTimestamp_Successfully()
+        {
+            //Arrange
+            var fromDateTime = new DateTime(2019, 11, 14, 19, 44, 45, 899);
+
+            //Act
+            var result = Timestamp.FromDateTime(fromDateTime);
+
+            //Assert
+            Assert.Equal(20191114194445, result.dateTime);
+            Assert.Equal(899, result.milliseconds);
+        }
+
+        [Fact]
+        public void ToDateTimeConvertsTimestampToDateTime_Successfully()
+        {
+            //Arrange
+            long dateTimeTimestamp = 20191114194445;
+            int milliseconds = 899;            
+
+            //Act
+            var result = Timestamp.ToDateTime(dateTimeTimestamp, milliseconds);
+
+            //Assert
+            Assert.Equal(2019, result.Year);
+            Assert.Equal(11, result.Month);
+            Assert.Equal(14, result.Day);
+            Assert.Equal(19, result.Hour);
+            Assert.Equal(44, result.Minute);
+            Assert.Equal(45, result.Second);
+            Assert.Equal(899, result.Millisecond);
+        }
+    }
+}
---
+++ InvincibleTraderExpertAdvisor/Timestamp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InvincibleTraderExpertAdvisor
+{
+    public class Timestamp
+    {
+        public static (long dateTime, int milliseconds) FromDateTime(DateTime fromDateTime)
+        {
+            return (GetTimestampDateTime(fromDateTime), fromDateTime.Millisecond);
+        }
+
+        public static DateTime ToDateTime(long timestampDateTime, int timestampMilliseconds)
+        {
+            int yearPart = (int)(timestampDateTime / 10000000000);
+            int yearMonthPart = (int)(timestampDateTime / 100000000);
+            int yearMonthDayPart = (int)(timestampDateTime / 1000000);
+            int hourMinSecPart = (int)(timestampDateTime -(yearMonthDayPart * 1000000L));
+            int hourPart = hourMinSecPart / 10000;
+            int hourMinPart = hourMinSecPart / 100;
+            return new DateTime(yearPart, yearMonthPart - (yearPart * 100), yearMonthDayPart - (yearMonthPart * 100), hourPart, hourMinPart - (hourPart * 100), hourMinSecPart - (hourMinPart * 100), timestampMilliseconds);
+        }
+
+        private static long GetTimestampDateTime(DateTime dateTime)
+        {            
+            return (dateTime.Year * 10000000000) + (dateTime.Month * 100000000) + (dateTime.Day * 1000000) + (dateTime.Hour * 10000) + (dateTime.Minute * 100) + (dateTime.Second);
+        }
+    }
+}
